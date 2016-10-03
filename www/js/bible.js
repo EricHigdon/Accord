fad5cd5ab4dd7def42897b7f1ba2fc3a1de18445
Eric Higdon <mrbassman.10@gmail.com>
2016-10-02 23:59:48 -0400

    Bible updates

--- www/js/bible.js
+++ www/js/bible.js
@@ -4,16 +4,14 @@ function get_bible(){
             passage = element.attr('data-book');
         if (element.attr('data-chapter') != null)
             passage += element.attr('data-chapter');
-        if (element.attr('data-verse-verse') != null)
-            passage += ':'+element.attr('data-verse-verse');
-        console.log(passage);
+        if (element.attr('data-verse') != null)
+            passage += ':'+element.attr('data-verse');
         $.ajax({
             url:'http://getbible.net/json',
             dataType: 'jsonp',
             data: 'v=kjv&p='+passage,
             jsonp: 'getbible',
             success:function(json){
-                console.log(json);
                 // set text direction
                 if (json.direction == 'RTL'){
                     var direction = 'rtl';
@@ -24,38 +22,36 @@ function get_bible(){
                 if (json.type == 'verse'){
                     var output = '';
                         $.each(json.book, function(index, value) {
-                            output += '<center><b>'+value.book_name+' '+value.chapter_nr+'</b></center><br/><p class="'+direction+'">';
+                            output += '<center><b>'+value.book_name+' '+value.chapter_nr+'</b></center>';
                             $.each(value.chapter, function(index, value) {
+                                output += '<p class="'+direction+'">';
                                 output += '  <sup class="ltr">' +value.verse_nr+ '</sup>  ';
                                 output += value.verse;
-                                output += '<br/>';
+                                output += '</p>';
                             });
-                            output += '</p>';
                         });
                     element.html(output);  // <---- this is the div id we update
                 } else if (json.type == 'chapter'){
-                    var output = '<center><b>'+json.book_name+' '+json.chapter_nr+'</b></center><br/><p class="'+direction+'">';
+                    var output = '<center><b>'+json.book_name+' '+json.chapter_nr+'</b></center>';
                     $.each(json.chapter, function(index, value) {
+                        output += '<p class="'+direction+'">';
                         output += '  <sup class="ltr">' +value.verse_nr+ '</sup>  ';
                         output += value.verse;
-                        output += '<br/>';
+                        output += '</p>';
                     });
-                    output += '</p>';
                     element.html(output);  // <---- this is the div id we update
                 } else if (json.type == 'book'){
                     var output = '';
                     $.each(json.book, function(index, value) {
-                        output += '<center><b>'+json.book_name+' '+value.chapter_nr+'</b></center><br/><p class="'+direction+'">';
+                        output += '<center><b>'+json.book_name+' '+value.chapter_nr+'</b></center>';
                         $.each(value.chapter, function(index, value) {
+                            output += '<p class="'+direction+'">';
                             output += '  <sup class="ltr">' +value.verse_nr+ '</sup>  ';
                             output += value.verse;
-                            output += '<br/>';
+                            output += '</p>';
                         });
-                        output += '</p>';
                     });
-                    if(addTo){
-                        element.html(output);  // <---- this is the div id we update
-                    }
+                    element.html(output);  // <---- this is the div id we update
                 }
             },
             error:function(e){
