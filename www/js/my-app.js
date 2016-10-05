1f1b1455af99271317a3ccbc6e243acbc1ce50ed
Eric Higdon <EricHigdon@users.noreply.github.com>
2016-10-05 08:05:54 -0400

    Update my-app.js

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -48,7 +48,7 @@ function setup() {
     });
     window.FirebasePlugin.grantPermission();
     myApp.onPageInit('*', function (page) {
-      window.FirebasePlugin.logEvent("page_view", {page: page.name});
+      window.FirebasePlugin.logEvent("page_view", page.name);
     });
     $$('body').on('beforeSubmit', '.ajax-submit', function(e) {
        myApp.showPreloader('Submitting');
@@ -56,7 +56,7 @@ function setup() {
     $$('body').on('submitted', '.ajax-submit', function (e) {
       var xhr = e.detail.xhr; // actual XHR object
       var data = JSON.parse(e.detail.data); // Ajax response from action file
-      window.FirebasePlugin.logEvent("submit_form", {form: $(this).find('#id_form').val()});
+      window.FirebasePlugin.logEvent("submit_form", $(this).find('#id_form').val());
       if(data.success) {
           $(this).html('<p>Thanks for contacting us!</p>')
       }
