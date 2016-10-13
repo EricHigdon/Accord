f3740869999e601ebe10565ccef1b5a6579c7883
Eric Higdon <mrbassman.10@gmail.com>
2016-10-12 22:13:48 -0400

    Update notification

--- platforms/browser/www/js/my-app.js
+++ platforms/browser/www/js/my-app.js
@@ -255,7 +255,7 @@ function setupNotifications() {
 
     push.on('notification', function(data) {
         console.log('notification event');
-        myApp.confirm(data.message, ['Update Available', updateBulletin()])
+        myApp.confirm(data.message, ['Update Available', updateBulletin]);
    });
 }
 
